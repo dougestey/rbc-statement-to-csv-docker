87e400a8de5b87f837c2bb203913da715425aa42
Doug Estey <doug.estey@gmail.com>
2023-05-01 21:19:04 -0300

    use python3

--- convert.sh
+++ convert.sh
@@ -11,12 +11,12 @@ PDF_FILES=$(find -H . -type f -regextype posix-extended -regex '\./.+\.pdf' -pri
 for PDF in ${PDF_FILES};
 do
 	XML_FILE=$(basename -s .pdf "${PDF}")
-	python "${PDF2TXT}" -o "${XML_FILE}.xml" "${PDF}" 
+	python3 "${PDF2TXT}" -o "${XML_FILE}.xml" "${PDF}"
 done
 
 XML_FILES=($(find -H . -maxdepth 1 -type f -regextype posix-extended -regex '\./.+\.xml' -print))
 
 echo "${XML_FILES[@]}"
-python convert.py "${OUTPUT_FILE}" "${XML_FILES[@]}"
+python3 convert.py "${OUTPUT_FILE}" "${XML_FILES[@]}"
 
 rm -rf *.xml
